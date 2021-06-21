f07b4306d7871d14f0a6e645b9612b8a4a42dc30
sam-in95 <qlrkqnf@naver.com>
2021-06-21 19:29:20 +0900

    add js

--- dokdo/script.js
+++ dokdo/script.js
@@ -11,3 +11,23 @@ mainHeaderHover.addEventListener('mouseleave', function () {
   mainHeader.style.display = 'block';
 
 });
+
+$('a').click(function (e) {
+  e.preventDefault();
+});
+
+let noticeBtn = $('.notice_btn'),
+  noticeCon = $('.notice_con');
+
+noticeBtn.click(function () {
+  var target = $(this);
+  var index = target.index();
+
+  noticeBtn.removeClass('notice_btn_click');
+  noticeBtn.addClass('notice_btn_noclick');
+  target.removeClass('notice_btn_noclick');
+  target.addClass('notice_btn_click');
+
+  noticeCon.css({ 'display': 'none', 'margin-left': '300px' });
+  noticeCon.eq(index).fadeIn().css({ 'margin-left': '350px', 'transition': '0.5s' });
+});
